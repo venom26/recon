b425e4f4956e996db5bc2ee8fa6ca0565c418216
venom26 <54362015+venom26@users.noreply.github.com>
2019-12-09 12:49:49 -0600

    Update sub_recon.sh

--- sub_recon.sh
+++ sub_recon.sh
@@ -12,10 +12,13 @@ curl -ss https://dns.bufferover.run/dns?q=.$1 | jq '.FDNS_A[]' | sed 's/^\".*.,/
 #running certspotter
 curl -ss https://certspotter.com/api/v0/certs\?domain\=$1 | jq '.[].dns_names[]' | sed 's/\"//g' | sed 's/\*\.//g' | tee -a $CUR_DIR/domains.txt
 
+cat $CUR_DIR/domains.txt | sort -u | tee -a $CUR_DIR/domains2.txt
+mv $CUR_DIR/domains2.txt $CUR_DIR/domains.txt
+
 echo -e "Starting Bruteforce\n"
 cat domains.txt | dnsgen - > brute.txt
 massdns -r ~/tools/subdomain_bruteforce/subbrute/resolvers.txt -t A -o J -w brute.json brute.txt
-cat brute.json  | grep -v "awsdns-hostmaster.amazon.com\|coby.ns.cloudflare.com." | jq ".query_name" | sed 's/^\"\|.\"$//g' | sort -u > brute_output.txt
+cat brute.json  | grep -v "awsdns-hostmaster.amazon.com\|coby.ns.cloudflare.com." | jq ".query_name" | sed 's/^\"\|.\"$//g' | grep $1 | sort -u > brute_output.txt
 cat brute_output.txt domains.txt | uniq | sort -u > output.txt
 
 
@@ -27,5 +30,6 @@ cat output.txt | httprobe | tee -a $CUR_DIR/alive.txt
 #formatting the data to json
 #cat $CUR_DIR/alive.txt | python -c "import sys; import json; print (json.dumps({'domains':list(sys.stdin)}))" > $CUR_DIR/alive.json
 #cat $CUR_DIR/domains.txt | python -c "import sys; import json; print (json.dumps({'domains':list(sys.stdin)}))" > $CUR_DIR/domains.json
+
 
 
