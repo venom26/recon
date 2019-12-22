fad6a6ccd49657072659f7931eacfcd8616415d1
venom26 <54362015+venom26@users.noreply.github.com>
2019-12-22 11:07:48 -0600

    Update sub_recon.sh

--- sub_recon.sh
+++ sub_recon.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 #starting sublist3r
+echo "######Starting Sublist3r######"
 CUR_DIR=$(pwd)
 sublist3r -d $1 -v -o $CUR_DIR/domains.txt
 
+#starting crtsh
+echo "######Starting crtsh######"
+crtsh $1 | tee -a $CUR_DIR/domains.txt
+
 #running assetfinder
+echo "######Starting assetfinder######"
 assetfinder --subs-only $1 | tee -a $CUR_DIR/domains.txt
 
 #running bufferover
+echo "######Starting bufferover######"
 curl -ss https://dns.bufferover.run/dns?q=.$1 | jq '.FDNS_A[]' | sed 's/^\".*.,//g' | sed 's/\"$//g'  | sort -u | tee -a $CUR_DIR/domains.txt
 
 #running certspotter
+echo "######Starting certspotter######"
 curl -ss https://certspotter.com/api/v0/certs\?domain\=$1 | jq '.[].dns_names[]' | sed 's/\"//g' | sed 's/\*\.//g' | tee -a $CUR_DIR/domains.txt
 
-cat $CUR_DIR/domains.txt | sort -u | tee -a $CUR_DIR/domains2.txt
+#running amass
+echo "######Running Amass######"
+amass enum -active -d $1 -ip | tee -a $CUR_DIR/amass_output.txt
+cat $CUR_DIR/amass_output.txt | awk '{print $1}' | tee -a $CUR_DIR/domains.txt
+
+cat $CUR_DIR/domains.txt |grep $1 | sort -u | tee -a $CUR_DIR/domains2.txt
 mv $CUR_DIR/domains2.txt $CUR_DIR/domains.txt
 
-echo -e "Starting Bruteforce\n"
+echo -e "######Starting Bruteforce######\n"
 cat domains.txt | dnsgen - > brute.txt
 massdns -r ~/tools/subdomain_bruteforce/subbrute/resolvers.txt -t A -o J -w brute.json brute.txt
 cat brute.json  | grep -v "awsdns-hostmaster.amazon.com\|coby.ns.cloudflare.com." | jq ".query_name" | sed 's/^\"\|.\"$//g' | grep $1 | sort -u > brute_output.txt
