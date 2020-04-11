1569e710cffbbe1962c9dd848f30bdcfa55cbb88
venom26 <54362015+venom26@users.noreply.github.com>
2020-04-11 14:05:25 +0530

    Update ultimate_recon.sh

--- ultimate_recon.sh
+++ ultimate_recon.sh
@@ -36,13 +36,13 @@ echo -e "${BLUE}[*] Check the list of IP addresses at $IP_PATH/final-ips.txt${RE
 
 echo "Starting Nuclei"
 mkdir nuclei_op
-nuclei -l alive.txt -t "~/tools/nuclei-templates/cves/*.yaml" -c 60 -o nuclei_op/cves.txt
-nuclei -l alive.txt -t "~/tools/nuclei-templates/files/*.yaml" -c 60 -o nuclei_op/files.txt
-nuclei -l alive.txt -t "~/tools/nuclei-templates/panels/*.yaml" -c 60 -o nuclei_op/panels.txt
-nuclei -l alive.txt -t "~/tools/nuclei-templates/security-misconfiguration/*.yaml" -c 60 -o nuclei_op/security-misconfiguration.txt
-nuclei -l alive.txt -t "~/tools/nuclei-templates/technologies/*.yaml" -c 60 -o nuclei_op/technologies.txt
-nuclei -l alive.txt -t "~/tools/nuclei-templates/tokens/*.yaml" -c 60 -o nuclei_op/tokens.txt
-nuclei -l alive.txt -t "~/tools/nuclei-templates/vulnerabilities/*.yaml" -c 60 -o nuclei_op/vulnerabilities.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/cves/*.yaml" -c 60 -o nuclei_op/cves.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/files/*.yaml" -c 60 -o nuclei_op/files.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/panels/*.yaml" -c 60 -o nuclei_op/panels.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/security-misconfiguration/*.yaml" -c 60 -o nuclei_op/security-misconfiguration.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/technologies/*.yaml" -c 60 -o nuclei_op/technologies.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/tokens/*.yaml" -c 60 -o nuclei_op/tokens.txt
+nuclei -l alive.txt -t "/root/tools/nuclei-templates/vulnerabilities/*.yaml" -c 60 -o nuclei_op/vulnerabilities.txt
 
 echo "Now looking for CORS misconfiguration"
 python3 ~/tools/Corsy/corsy.py -i alive.txt -t 40 | tee -a corsy_op.txt
