eb37bd3af35ca4ed62905f44091b06475a984264
venom26 <54362015+venom26@users.noreply.github.com>
2020-04-20 10:15:26 +0530

    Update sub_recon.sh

--- sub_recon.sh
+++ sub_recon.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+
+CUR_DIR=$(pwd)
+#starting findomain
+echo "######Starting Findomain#########"
+findomain -t $1 -u $CUR_DIR/domains.txt
+
 #starting sublist3r
 echo "######Starting Sublist3r######"
-CUR_DIR=$(pwd)
 sublist3r -d $1 -v -o $CUR_DIR/domains.txt
-
-#starting crtsh
-echo "######Starting crtsh######"
-crtsh $1 | tee -a $CUR_DIR/domains.txt
 
 #running assetfinder
 echo "######Starting assetfinder######"
@@ -16,33 +17,22 @@ assetfinder --subs-only $1 | tee -a $CUR_DIR/domains.txt
 echo "######Starting bufferover######"
 curl -ss https://dns.bufferover.run/dns?q=.$1 | jq '.FDNS_A[]' | sed 's/^\".*.,//g' | sed 's/\"$//g'  | sort -u | tee -a $CUR_DIR/domains.txt
 
-#running certspotter
-echo "######Starting certspotter######"
-curl -ss https://certspotter.com/api/v0/certs\?domain\=$1 | jq '.[].dns_names[]' | sed 's/\"//g' | sed 's/\*\.//g' | tee -a $CUR_DIR/domains.txt
-
 #running amass
 echo "######Running Amass######"
-amass enum -active -d $1 -ip | tee -a $CUR_DIR/amass_output.txt
-cat $CUR_DIR/amass_output.txt | awk '{print $1}' | tee -a $CUR_DIR/domains.txt
+amass enum -d $1  | tee -a $CUR_DIR/domains.txt
 
-cat $CUR_DIR/domains.txt |grep $1 | sort -u | tee -a $CUR_DIR/domains2.txt
-mv $CUR_DIR/domains2.txt $CUR_DIR/domains.txt
+cat $CUR_DIR/domains.txt | sort -u | tee -a all.txt
 
-echo -e "######Starting Bruteforce######\n"
-cat domains.txt | dnsgen - > brute.txt
-massdns -r ~/tools/subdomain_bruteforce/subbrute/resolvers.txt -t A -o J -w brute.json brute.txt
-cat brute.json  | grep -v "awsdns-hostmaster.amazon.com\|coby.ns.cloudflare.com." | jq ".query_name" | sed 's/^\"\|.\"$//g' | grep $1 | sort -u > brute_output.txt
-cat brute_output.txt domains.txt | uniq | sort -u > output.txt
+#starting shuffledns
+echo "#####starting shuffledns#####"
+shuffledns -d $1 -w ~/tools/subdomains.txt -r ~/tools/subbrute/resolvers.txt | tee -a bruteforced.txt
 
+cat bruteforced.txt | tee -a all.txt 
 
-#removing duplicate entries
-sort -u output.txt -o $CUR_DIR/domains.txt
-#checking for alive domains
-echo "Checking for alive domains"
-cat output.txt | httprobe | tee -a $CUR_DIR/alive.txt
-#formatting the data to json
-#cat $CUR_DIR/alive.txt | python -c "import sys; import json; print (json.dumps({'domains':list(sys.stdin)}))" > $CUR_DIR/alive.json
-#cat $CUR_DIR/domains.txt | python -c "import sys; import json; print (json.dumps({'domains':list(sys.stdin)}))" > $CUR_DIR/domains.json
+shuffledns -d $1 -list all.txt -r ~/tools/subbrute/resolvers.txt | tee -a resolved.txt
 
-
-
+cat all.txt | httprobe -c 40 | tee -a alive2.txt
+cat resolved.txt | httprobe -c 40 | tee -a alive2.txt
+cat alive2.txt | sort -u |tee -a alive.txt
+rm -rf alive2.txt 
+rm -rf domains.txt
