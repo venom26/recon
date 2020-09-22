64be36799f03c23f7a497b28a77f167993d78cd8
venom26 <54362015+venom26@users.noreply.github.com>
2020-09-22 15:50:36 +0530

    Update new_machine.sh

--- new_machine.sh
+++ new_machine.sh
@@ -8,7 +8,7 @@ sudo apt install -y dnsutils
 sudo apt-get install -y libcurl4-openssl-dev
 sudo apt-get install -y libssl-dev
 sudo apt-get install -y jq
-sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev
+sudo apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev -y
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
 source ~/.rvm/scripts/rvm
