68728f8a88adca5b32bdfb4bccf69cfafc137af2
venom26 <54362015+venom26@users.noreply.github.com>
2020-09-22 17:53:57 +0530

    Update new_machine.sh

--- new_machine.sh
+++ new_machine.sh
@@ -52,7 +52,7 @@ select choice in "${choices[@]}"; do
                                         echo "Installing Golang"
                                         wget https://dl.google.com/go/go1.14.linux-amd64.tar.gz
                                         sudo tar -xvf go1.14.linux-amd64.tar.gz
-                                        sudo mv go /usr/local
+                                        sudo mv go /usr/local/
                                         export GOROOT=/usr/local/go
                                         export GOPATH=$HOME/go
                                         export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
