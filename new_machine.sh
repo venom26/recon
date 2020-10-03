b0348301666b9b6e9a49a2d0baa389abd1767576
venom26 <54362015+venom26@users.noreply.github.com>
2020-10-03 11:29:27 +0530

    Update new_machine.sh

--- new_machine.sh
+++ new_machine.sh
@@ -367,6 +367,9 @@ wget https://raw.githubusercontent.com/assetnote/commonspeak2-wordlists/master/s
 wget https://gist.githubusercontent.com/jhaddix/86a06c5dc309d08580a018c66354a056/raw/96f4e51d96b2203f19f6381c8c545b278eaa0837/all.txt
 mv all.txt jhaddix_all.txt
 wget https://raw.githubusercontent.com/BBerastegui/fresh-dns-servers/master/resolvers.txt -O resolvers.txt
+echo -e "${LIGHT_YELLOW}Setting ulimit to 100000 ${LIGHT_GREEN}( so as to make ffuf work fine with higher number of threads ) ${NORMAL}"
+echo "ulimit -n 100000" >> ~/.bashrc
+source ~/.bashrc
 source $HOME/.cargo/env
 echo -e "\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in ~/tools"
 ls -la
