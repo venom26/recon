f0dd083016127376395d1a12fab8d047b2e45483
venom26 <54362015+venom26@users.noreply.github.com>
2020-10-10 23:37:12 +0530

    Update brute.py

--- brute.py
+++ brute.py
@@ -5,3 +5,4 @@ wordlist = open('/home/ubuntu/tools/subdomains.txt').read().split('\n')
 for word in wordlist:
     if not word.strip():
         continue
+    print('{}.{}\n'.format(word.strip(), scope))
