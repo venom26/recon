818b4fa195c6031d9ff258eb7bb7c662a8197677
venom26 <54362015+venom26@users.noreply.github.com>
2020-10-30 18:12:54 +0530

    Update crtndstry_new.sh

--- crtndstry_new.sh
+++ crtndstry_new.sh
@@ -13,12 +13,12 @@ fi
 certdata(){
 	#give it patterns to look for within crt.sh for example %api%.site.com
 	declare -a arr=("api" "corp" "dev" "uat" "test" "stag" "sandbox" "prod" "internal" "qa" "admin" "backend" "stage" "web" "web-qa" "web-dev" "uat" "beta")
-	for i in "${arr[@]}"
-	do
+	#for i in "${arr[@]}"
+	#do
 		#get a list of domains based on our patterns in the array
 		echo "Running crt.sh"
 		crtsh=$(curl -s https://crt.sh\?q\=%25.$1\&output\=json | jq . | grep 'name_value' | awk '{print $2}' | sed -e 's/"//g'| sed -e 's/,//g' |  awk '{gsub(/\\n/,"\n")}1' | grep -iv '*' |grep -iv '@' | grep -iv '\--' | sort -u | tee -a rawdata/$1-crtsh.txt )
-	done
+	#done
 		#get a list of domains from certspotter
 		echo "Running Certspotter"
 		certspotter=$(curl -s https://certspotter.com/api/v0/certs\?domain\=$1 | jq '.[].dns_names[]' | sed 's/\"//g' | sed 's/\*\.//g' | sort -u | grep -w $1\$ | tee rawdata/$1-certspotter.txt)
