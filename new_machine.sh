95d7400579dd1a2745d700a1d41fb546d05c0da4
venom26 <54362015+venom26@users.noreply.github.com>
2021-03-29 11:47:55 +0530

    Update new_machine.sh

--- new_machine.sh
+++ new_machine.sh
@@ -396,6 +396,7 @@ wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/
 wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/spring-boot.txt
 wget https://raw.githubusercontent.com/milo2012/pathbrute/master/defaultPaths.txt
 wget https://raw.githubusercontent.com/milo2012/pathbrute/master/cvePaths.txt
+wget https://gist.githubusercontent.com/jhaddix/b80ea67d85c13206125806f0828f4d10/raw/c81a34fe84731430741e0463eb6076129c20c4c0/content_discovery_all.txt -O all.txt
 cp ~/tools/recon/apiwords.txt .
 cd -
 cd /usr/share/nmap/scripts/
