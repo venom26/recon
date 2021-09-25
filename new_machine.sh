92ac4f761d8691d1b8f6e01b5d34a9eb5ed546db
venom26 <54362015+venom26@users.noreply.github.com>
2021-09-25 18:48:51 +0530

    Update new_machine.sh

--- new_machine.sh
+++ new_machine.sh
@@ -459,6 +459,7 @@ cp ~/tools/dirsearch/db/apiwords.txt .
 cp ~/tools/recon/ffuf_extension.txt .
 wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/DNS/deepmagic.com-prefixes-top50000.txt
 wget https://raw.githubusercontent.com/assetnote/commonspeak2-wordlists/master/wordswithext/php.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/common.txt
 wget https://raw.githubusercontent.com/danielmiessler/SecLists/c196a6e62d0b63d6be0c84e6fa224352ea5949df/Discovery/Web-Content/SVNDigger/cat/Language/js.txt
 wget https://raw.githubusercontent.com/r3s-ryan/R3S-CTF/a2733be0b3fdc553930493ae164256e3a30f40aa/SecLists/Discovery/Web-Content/quickhits.txt
 wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/raft-large-files.txt
